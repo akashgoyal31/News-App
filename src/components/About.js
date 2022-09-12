b806f0ecb6be8c5ad97d39cc8afeb75729646200
Akash goyal <goyalakash3107@gmail.com>
2022-09-12 18:45:45 +0530

    news

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div style={{marginTop :"70px", fontSize: "30px " , fontStyle :"italic"}}>
+      <p>News website is a free website and easily accessible from any device ( like mobile phone , laptop, computer etc . )  
+      </p>
+      <p>
+        You can choose your interested field and take your daily dose of news 
+      </p>
+    </div>
+  )
+}
